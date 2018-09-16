e3fc36be344126375870dc7704fa53e05f5879d1
Kevin Mannix <kmannix@bu.edu>
2018-09-16 09:42:40 -0400

    setting MSB for 7-bit chunks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env; 
 use std::process;
-use std::slice;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -9,32 +8,41 @@ fn main() {
     process::exit(1)
   });
 
-  println!("Integer: {:?}", to_bitv(config.integ));
+  println!("Integer: {:?}", to_bitv_reversed(config.integ));
   
-  println!("Chunks: {:?}", to_chunks(&to_bitv(config.integ)))
+  println!("Chunks: {:?}", to_chunks(&to_bitv_reversed(config.integ)))
 }
 
 fn run(config: Config) {
 
 }
 
-fn to_chunks<'a>(bits: &'a Vec<bool>) -> &[bool] {
-  return bits.chunks(7).next().unwrap();
+fn to_chunks<'a>(bits: &'a Vec<bool>) -> Vec<Vec<bool>> {
+  let chunks = bits.chunks(7).enumerate();
+  return chunks.clone().map(|(i, chunk)| {
+    let mut vec = Vec::with_capacity(8);
+    for x in 0..7 {
+      vec.push(
+        if x < chunk.len() { chunk[x] } else { false }
+      )
+    }
+    vec.push(i == chunks.len() - 1); // set MSB to indicate whether more bytes are included
+    return vec;
+  }).collect();
 }
 
 
-fn to_bitv(int: u32) -> Vec<bool> {
-  fn to_bitv_acc(mut acc: Vec<bool>, int: u32) -> Vec<bool> {
+fn to_bitv_reversed(int: u32) -> Vec<bool> {
+  fn to_bitv_reversed_acc(mut acc: Vec<bool>, int: u32) -> Vec<bool> {
     if int == 0 {
-      acc.reverse();
       acc
     } else {
       acc.push(int % 2 == 1);
-      to_bitv_acc(acc, int / 2)
+      to_bitv_reversed_acc(acc, int / 2)
     }
   }
 
-  to_bitv_acc(Vec::with_capacity(32), int)
+  to_bitv_reversed_acc(Vec::with_capacity(32), int)
 }
 
 

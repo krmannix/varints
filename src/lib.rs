4e3977dbe378b8e841ac0f28e676985a749ae3a1
Kevin Mannix <kmannix@bu.edu>
2018-10-13 21:20:31 -0400

    slight refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::char;
 
 pub fn to_hex(int: u32) -> Vec<char> {
-  let bitv_reversed = to_bitv_reversed(int);
-  let bytev = to_bytev(&bitv_reversed);
-  return to_hexv(&bytev);
+  let bits_reversed = int_to_bits_reversed(int);
+  let bytes = bits_reversed_to_bytes(&bits_reversed);
+  return bytes_to_hex(&bytes);
 }
 
-fn to_hexv<'a>(bytes: &'a Vec<Vec<bool>>) -> Vec<char> {
+fn bytes_to_hex(bytes: &Vec<Vec<bool>>) -> Vec<char> {
   return bytes.iter().flat_map(|bitv| {
     return bitv.chunks(4).map(|bits| {
       return nibble_to_hex(bits.to_vec());
@@ -15,51 +15,43 @@ fn to_hexv<'a>(bytes: &'a Vec<Vec<bool>>) -> Vec<char> {
 }
 
 fn nibble_to_hex(nibble: Vec<bool>) -> char {
-  let int_nibble: Vec<u32> = nibble.iter().map(|&bit| if bit { 1 } else { 0 }).collect();
-  let num = int_nibble[0] * 8 + int_nibble[1] * 4 + int_nibble[2] * 2 + int_nibble[3] * 1;
+  let num = nibble.iter().enumerate().fold(0, |acc, (i, &bit)| {
+    acc + if bit { 2u8.pow((nibble.len() - i - 1) as u32) } else { 0 }
+  });
 
   if num < 10 {
-    return char::from_digit(num, 10).unwrap(); 
+    return char::from_digit(num as u32, 10).unwrap(); 
   } else {
-    return match num {
-      10 => 'A',
-      11 => 'B',
-      12 => 'C',
-      13 => 'D',
-      14 => 'E',
-      15 => 'F',
-      _  => 'X',
-    }
+    return (num + 55) as char;
   }
 }
 
-fn to_bytev<'a>(bits: &'a Vec<bool>) -> Vec<Vec<bool>> {
-  let last_chunk_index = bits.len() / 7;
-  return bits.chunks(7).enumerate().map(|(i, chunk)| {
-    let mut byte = Vec::with_capacity(8);
+fn bits_reversed_to_bytes(bits_reversed: &Vec<bool>) -> Vec<Vec<bool>> {
+  let last_chunk_index = bits_reversed.len() / 7;
+
+  return bits_reversed.chunks(7).enumerate().map(|(i, chunk)| {
+    let mut byte: Vec<bool> = Vec::with_capacity(8);
     for x in 0..7 {
-      byte.push(
-        if x < chunk.len() { chunk[x] } else { false }
-      )
+      byte.push(if x < chunk.len() { chunk[x] } else { false })
     }
     byte.push(i != last_chunk_index); // set MSB to indicate whether more bytes are included
     byte.reverse();
-    return byte;
+    return byte.to_vec();
   }).collect();
 }
 
 
-fn to_bitv_reversed(int: u32) -> Vec<bool> {
-  fn to_bitv_reversed_acc(mut acc: Vec<bool>, int: u32) -> Vec<bool> {
+fn int_to_bits_reversed(int: u32) -> Vec<bool> {
+  fn int_to_bits_reversed_acc(mut acc: Vec<bool>, int: u32) -> Vec<bool> {
     if int == 0 {
       acc
     } else {
       acc.push(int % 2 == 1);
-      to_bitv_reversed_acc(acc, int / 2)
+      int_to_bits_reversed_acc(acc, int / 2)
     }
   }
 
-  to_bitv_reversed_acc(Vec::with_capacity(32), int)
+  int_to_bits_reversed_acc(Vec::with_capacity(32), int)
 }
 
 #[cfg(test)]
